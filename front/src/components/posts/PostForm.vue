7e179239ac28bc8b653afb11b4b91727f8bc33bc
Max Tsilyurik <tsilyurik.maxim@gmail.com>
2020-12-03 02:44:14 +0300

    поправлена форма добавления нового поста(мок)

--- front/src/components/posts/PostForm.vue
+++ front/src/components/posts/PostForm.vue
@@ -1,18 +1,50 @@
 <template>
-  <v-layout row>
+  <v-row class="shrink">
     <v-text-field
         class="mx-4"
         placeholder="Что нового?"
         v-model="text"
         style="max-width: 700px"
-        readonly
+        @click="expand = !expand"
     />
-  </v-layout>
+    <v-expand-transition>
+      <v-card
+          v-show="expand"
+          height="110px"
+          width="700px"
+          class="mx-4"
+      >
+        <v-file-input
+            class="mx-6"
+            counter
+            multiple
+            width="690px"
+        ></v-file-input>
+        <v-btn
+            style="position: relative;
+                left: 50%;
+                transform: translate(-50%, 0);"
+            color="primary"
+            type="submit"
+            text
+            @click="submit"
+        >
+          Отправить
+        </v-btn>
+      </v-card>
+    </v-expand-transition>
+  </v-row>
+
 </template>
 
 <script>
 export default {
-  name: "PostForm"
+  name: "PostForm",
+  data (){
+    return {
+      expand: false
+    }
+  }
 }
 </script>
 
