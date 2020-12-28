aa14b3f3fb57b4e9c832fa6511781946e047ff98
Max Tsilyurik <tsilyurik.maxim@gmail.com>
2020-12-28 14:33:12 +0300

    fix

--- front/src/layouts/default-layout.vue
+++ front/src/layouts/default-layout.vue
@@ -15,14 +15,6 @@
               </v-toolbar-title>
 
               <v-spacer></v-spacer>
-
-              <v-btn icon color="white">
-                <v-icon>mdi-magnify</v-icon>
-              </v-btn>
-
-              <v-btn icon color="white">
-                <v-icon>mdi-dots-vertical</v-icon>
-              </v-btn>
             </v-app-bar>
           </v-main>
         </v-card>
